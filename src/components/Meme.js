53360422eab9094e2ef87093100255521ab5efeb
Zackary Springer <springerzack04@yahoo.com>
2023-10-16 14:41:25 -0700

    create-meme-component

---
+++ src/components/Meme.js
@@ -0,0 +1,14 @@
+export default function Meme() {
+    return (
+        <main className="main">
+            <form action="">
+                <div className="main-input">
+                    <input className="main-input-1" placeholder="Shut up" type="text"></input>
+                    <input className="main-input-2" placeholder="and take my money" type="text"></input>
+                </div>
+                <button className="main-button">Get a new meme image  🖼</button>
+            </form>
+            <img src="" alt="" />
+        </main>
+    )
+}
