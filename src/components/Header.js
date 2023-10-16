3cb3f5568e5abd3a4afb5097ea192af8bbc6c169
Zackary Springer <springerzack04@yahoo.com>
2023-10-16 12:12:04 -0700

    create-header-component

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+export default function Header() {
+    return (
+        <header>
+            <div className="logo">
+                <img src="./troll-face.png" alt="Troll Face"/>
+                <h2>Meme Generator</h2>
+            </div>
+            <h4 className="subtitle">React Course - Project 3</h4>
+        </header>
+    )
+}
