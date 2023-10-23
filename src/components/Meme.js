060478c259ed6f94877482b8eec715f743e77eb5
Zackary Springer <springerzack04@yahoo.com>
2023-10-23 14:00:30 -0700

    write random meme image function

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,14 +1,34 @@
+import { useState } from "react"
+import memesData from "../memesdata"
+
 export default function Meme() {
+
+    const [memeImage, setMemeImage] = useState("https://i.imgflip.com/3si4.jpg")
+    
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        setMemeImage(memesArray[randomNumber].url)
+    }
+
     return (
         <main className="main">
-            <form action="">
+            <div>
                 <div className="main-input">
-                    <input className="main-input-1" placeholder="Shut up" type="text"></input>
-                    <input className="main-input-2" placeholder="and take my money" type="text"></input>
+                    <input 
+                        className="main-input-1"
+                        placeholder="Shut up"
+                        type="text">
+                    </input>
+                    <input 
+                        className="main-input-2"
+                        placeholder="and take my money"
+                        type="text">
+                    </input>
                 </div>
-                <button className="main-button">Get a new meme image  🖼</button>
-            </form>
-            <img src="" alt="" />
+                <button className="main-button" onClick={getMemeImage}>Get a new meme image  🖼</button>
+            </div>
+            <img src={memeImage} alt="" className="main-img"/>
         </main>
     )
 }
